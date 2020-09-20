5ebf78e18713e3b0f120242fa2cdc6d9e11bd97b
cha-king <charlie.king4967@gmail.com>
2020-09-20 14:17:08 -0700

    Add index module content

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-auth = require('./auth.js')
+const express = require('express');
+const bodyParser = require('body-parser')
+const googleApp = require('./actions.js');
 
-auth.start()
+
+const port = process.env.EXPRESS_PORT;
+const google_client_id = process.env.GOOGLE_CLIENT_ID;
+const google_client_secret = process.env.GOOGLE_CLIENT_SECRET;
+const AUTH_CODE = 'AUTH'
+
+
+const app = express();
+
+const jsonParser = bodyParser.json();
+app.post('/fulfillment', jsonParser, googleApp);
+
+app.get('/auth', (req, res) => {
+    const redirect_uri = req.query.redirect_uri + `?code=${AUTH_CODE}&state=${req.query.state}`;
+
+    res.redirect(redirect_uri);
+});
+
+app.post('/token', (req, res) => {
+    const response = {
+        token_type: "Bearer",
+        access_token: "ACCESS_TOKEN",
+        refresh_token: "REFRESH_TOKEN",
+        expires_in: 300
+    }
+
+    res.json(response);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
