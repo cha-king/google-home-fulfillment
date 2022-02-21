faf39ae8faaeab99021d3304281bf89d53a337fc
Charlie King <charlie.king4967@gmail.com>
2022-02-20 18:35:31 -0800

    Add comments

--- src/oauth.ts
+++ src/oauth.ts
@@ -4,6 +4,19 @@ import express from 'express'
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
+/*
+There are some quirks to how we use OAuth here. The main takeaway is
+that we don't actually have users, since the fulfillment app isn't
+actually deployed. This being the case, we only really care about
+authenticating Google, which the client ID / secret is sufficient for.
+
+What this means for us is:
+    - We can pretty much stub out the auth endpoint
+    - We don't care about authorization code or refresh tokens
+    - We still want to generate and check short(ish)-lived auth tokens
+      but we can just assume client id / secret is enough to give this out
+*/
+
 
 const router = express.Router()
 
